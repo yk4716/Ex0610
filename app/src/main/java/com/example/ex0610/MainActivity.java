bfde663252c2852d1345527545b8b224c917390e
yk4716 <yk4716@bs.amalnet.k12.il>
2025-04-11 14:23:11 +0300

    I finished the code

--- app/src/main/java/com/example/ex0610/MainActivity.java
+++ app/src/main/java/com/example/ex0610/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.ex0610;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Switch;
+import android.widget.ToggleButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,10 +15,34 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    Button btn;
+    ToggleButton tb;
+    Switch swDN;
+    LinearLayout mainLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tb = (ToggleButton) findViewById(R.id.tb);
+        swDN = (Switch) findViewById(R.id.swDN);
+        btn = (Button) findViewById(R.id.btn);
+
+    }
+
+    public void go(View view) {
+        boolean switchState = swDN.isChecked();
+        boolean toggleState = tb.isChecked();
+
+        if (switchState && toggleState) {
+            mainLayout.setBackgroundColor(Color.RED);
+        } else if (switchState && !toggleState) {
+            mainLayout.setBackgroundColor(Color.BLUE);
+        } else if (!switchState && toggleState) {
+            mainLayout.setBackgroundColor(Color.GREEN);
+        } else {
+            mainLayout.setBackgroundColor(Color.YELLOW);
+        }
     }
 }
